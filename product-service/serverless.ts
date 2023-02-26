0ee23215492848c9041f337fedb3aa79efa62c1d
andrii.naumkyn <bobmads@gmail.com>
2023-02-27 01:43:26 +0200

    added swagger

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,23 +1,23 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import getProductsList from '@functions/getProductsList';
-import getProductById from '@functions/getProductById';
+import getProductsList from "@functions/getProductsList";
+import getProductById from "@functions/getProductById";
 
 const serverlessConfiguration: AWS = {
-  service: 'product-service',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-offline'],
+  service: "product-service",
+  frameworkVersion: "3",
+  plugins: ["serverless-auto-swagger", "serverless-esbuild", "serverless-offline"],
   provider: {
-    name: 'aws',
-    region: 'eu-west-1',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    region: "eu-west-1",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   // import the function via paths
@@ -28,11 +28,16 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
+    },
+    autoswagger: {
+      basePath: "/dev",
+      apiType: "http",
+      typefiles: ["./src/models/index.ts"],
     },
   },
 };
